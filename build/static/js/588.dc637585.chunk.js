f1e48101620e5725bdec609988dfdecce5ed7d12
Alex <nameaboutn@gmail.com>
2023-10-26 06:22:55 +0300

    First version

---
+++ build/static/js/588.dc637585.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkapp_1=self.webpackChunkapp_1||[]).push([[588],{1588:function(n,e,t){t.r(e),t.d(e,{default:function(){return r}});t(2791);var u=t(184),r=function(){return(0,u.jsx)("div",{children:"Settings"})}}}]);
+//# sourceMappingURL=588.dc637585.chunk.js.map
